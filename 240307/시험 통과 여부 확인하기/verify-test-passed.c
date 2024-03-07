914a17f1c0648a934818568305f50ecd4ebf1250
sijoon-sung <162120398+sijoon-sung@users.noreply.github.com>
2024-03-07 22:11:17 +0900

    Title: 시험 통과 여부 확인하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240307/시험 통과 여부 확인하기/verify-test-passed.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    if (a > 80) {
+        printf("pass");
+    }
+    else {
+        printf("%d more score",80-a);
+    }
+    return 0;
+}
